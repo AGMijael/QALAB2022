4788618cd5d540a23830df3c1d86ad6f5bbe7e64
AGMijael <agmijael@gmail.com>
2022-12-08 22:14:52 -0500

    agregando comentario en poogame

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        // editado por mijael
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
